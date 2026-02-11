7fdaf6b16e03f03c269ea4f24424f8d1bf1aca79
Rodrigo Rossi <rossi.rodrigo@outlook.com.br>
2026-02-10 21:53:54 -0300

    Change products exercise to add functions

--- exercises/pandas/products.py
+++ exercises/pandas/products.py
@@ -16,10 +16,16 @@ df_produtos = pd.DataFrame(
         "Preço": precos_produtos,
         "Itens Vendidos": itens_vendidos,
         "Avaliação": avaliacoes_produtos,
-        "Receita": precos_produtos * itens_vendidos,
     }
-)
+).set_index("Nome")
 
+# Filter eletronics and add 50% to the price
+df_produtos.loc[df_produtos["Categoria"] == "Eletrônicos", "Preço"] *= 1.5
+
+# Add revenue column
+df_produtos["Receita"] = df_produtos["Preço"] * df_produtos["Itens Vendidos"]
+
+# Filter by revenue and rating, sort by revenue
 revenue_filtered = df_produtos[
     (df_produtos["Receita"] > 100000) & (df_produtos["Avaliação"] > 4.0)
 ].sort_values(by="Receita", ascending=False)
@@ -29,8 +35,8 @@ print(revenue_filtered)
 print("\n")
 
 print("Primeiros 3 produtos com receita acima de 100.000 e avaliação acima de 4.0:")
-print(revenue_filtered.iloc[0:3])
+print(revenue_filtered.head(3))
 print("\n")
 
 print("Últimos 3 produtos com receita acima de 100.000 e avaliação acima de 4.0:")
-print(revenue_filtered.iloc[-3:])
+print(revenue_filtered.tail(3))
