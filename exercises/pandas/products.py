8cf6f6a38ac52dbfcdfc5264b8a98635af5b6cfe
Rodrigo Rossi <rossi.rodrigo@outlook.com.br>
2026-02-10 21:30:12 -0300

    Add Pandas product exercise

---
+++ exercises/pandas/products.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+nomes_produtos = [f"Produto {i+1}" for i in range(50)]
+categorias_produtos = np.random.choice(
+    ["Eletrônicos", "Livros", "Roupas", "Alimentos", "Brinquedos"], 50
+)
+precos_produtos = np.random.uniform(10.0, 500.0, 50).round(2)
+itens_vendidos = np.random.randint(1, 1000, 50)
+avaliacoes_produtos = np.random.uniform(1.0, 5.0, 50).round(1)
+
+df_produtos = pd.DataFrame(
+    {
+        "Nome": nomes_produtos,
+        "Categoria": categorias_produtos,
+        "Preço": precos_produtos,
+        "Itens Vendidos": itens_vendidos,
+        "Avaliação": avaliacoes_produtos,
+        "Receita": precos_produtos * itens_vendidos,
+    }
+)
+
+revenue_filtered = df_produtos[
+    (df_produtos["Receita"] > 100000) & (df_produtos["Avaliação"] > 4.0)
+].sort_values(by="Receita", ascending=False)
+
+print("Produtos com receita acima de 100.000 e avaliação acima de 4.0:")
+print(revenue_filtered)
+print("\n")
+
+print("Primeiros 3 produtos com receita acima de 100.000 e avaliação acima de 4.0:")
+print(revenue_filtered.iloc[0:3])
+print("\n")
+
+print("Últimos 3 produtos com receita acima de 100.000 e avaliação acima de 4.0:")
+print(revenue_filtered.iloc[-3:])
