aa9c35b008ef2073944dcfba74375668c11cda28
Rodrigo Rossi <rossi.rodrigo@outlook.com.br>
2026-02-22 11:19:38 -0300

    Make Groq work

---
+++ groq_test.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "You are a helpful and concise assistant."},
+        {
+            "role": "user",
+            "content": "What time is it in Brazil right now?",
+        },
+    ],
+    model="llama-3.3-70b-versatile",
+    temperature=0.5,
+    max_tokens=1024,
+)
+
+print(chat_completion.choices[0].message.content)
